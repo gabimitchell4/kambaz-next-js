cceadf2e42d46479211eed81f4a4b1b02ea2e5d5
gabimitchell4 <gabimitchell77@gmail.com>
2025-10-03 18:00:49 -0400

    a2 CSS and Bootstrap

--- app/Labs/Lab2/Float.tsx
+++ app/Labs/Lab2/Float.tsx
@@ -32,10 +32,10 @@ export default function Float() {
         looked up one of the more obscure Latin words, consectetur, from a Lorem
         Ipsum passage, and going through the cites of the word in classical
         literature, discovered the undoubtable source. Lorem Ipsum comes from
-        sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
+        sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The
         Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a
         treatise on the theory of ethics, very popular during the Renaissance.
-        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
+        The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes
         from a line in section 1.10.32. Lorem ipsum, dolor sit amet consectetur
         adipisicing elit. Eius hic Lorem Ipsum is simply dummy text of the
         printing and typesetting industry. Lorem Ipsum has been the industrys
@@ -89,10 +89,10 @@ export default function Float() {
         looked up one of the more obscure Latin words, consectetur, from a Lorem
         Ipsum passage, and going through the cites of the word in classical
         literature, discovered the undoubtable source. Lorem Ipsum comes from
-        sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
+        sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The
         Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a
         treatise on the theory of ethics, very popular during the Renaissance.
-        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
+        The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes
         from a line in section 1.10.32. Lorem ipsum, dolor sit amet consectetur
         adipisicing elit. Eius hic Lorem Ipsum is simply dummy text of the
         printing and typesetting industry. Lorem Ipsum has been the industrys
@@ -146,10 +146,10 @@ export default function Float() {
         looked up one of the more obscure Latin words, consectetur, from a Lorem
         Ipsum passage, and going through the cites of the word in classical
         literature, discovered the undoubtable source. Lorem Ipsum comes from
-        sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
+        sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The
         Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a
         treatise on the theory of ethics, very popular during the Renaissance.
-        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
+        The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes
         from a line in section 1.10.32. Lorem ipsum, dolor sit amet consectetur
         adipisicing elit. Eius hic Lorem Ipsum is simply dummy text of the
         printing and typesetting industry. Lorem Ipsum has been the industrys
@@ -203,10 +203,10 @@ export default function Float() {
         looked up one of the more obscure Latin words, consectetur, from a Lorem
         Ipsum passage, and going through the cites of the word in classical
         literature, discovered the undoubtable source. Lorem Ipsum comes from
-        sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
+        sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The
         Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a
         treatise on the theory of ethics, very popular during the Renaissance.
-        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
+        The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes
         from a line in section 1.10.32. Lorem ipsum, dolor sit amet consectetur
         adipisicing elit. Eius hic Lorem Ipsum is simply dummy text of the
         printing and typesetting industry. Lorem Ipsum has been the industrys
