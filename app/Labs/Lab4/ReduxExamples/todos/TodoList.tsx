c46d2739c0cd55faaa3aee4f2f350db4d49cc116
gabimitchell4 <gabimitchell77@gmail.com>
2025-11-03 17:34:59 -0500

    a4

--- app/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
+++ app/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
@@ -12,8 +12,8 @@ export default function TodoList() {
       <h2>Todo List</h2>
       <ListGroup>
         <TodoForm />
-        {todos.map((todo: any) => (
-          <TodoItem todo={todo} />
+        {todos.map((todo: any, i: number) => (
+          <TodoItem key={i} todo={todo} />
         ))}
       </ListGroup>
       <hr />
