324df1a6d35b1283d9cd979337819f7394f7123b
gabimitchell4 <gabimitchell77@gmail.com>
2025-11-13 14:34:03 -0500

    a5

--- app/(Kambaz)/Account/client.tsx
+++ app/(Kambaz)/Account/client.tsx
@@ -5,7 +5,7 @@ export const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 export const USERS_API = `${HTTP_SERVER}/api/users`;
 
 export const signin = async (credentials: any) => {
-  const response = await axiosWithCredentials.post(
+  const response = await axios.post(
     `${USERS_API}/signin`,
     credentials
   );
@@ -13,21 +13,21 @@ export const signin = async (credentials: any) => {
 };
 
 export const signup = async (user: any) => {
-  const response = await axiosWithCredentials.post(`${USERS_API}/signup`, user);
+  const response = await axios.post(`${USERS_API}/signup`, user);
   return response.data;
 };
 
 export const updateUser = async (user: any) => {
-  const response = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, user);
+  const response = await axios.put(`${USERS_API}/${user._id}`, user);
   return response.data;
 };
 
 export const profile = async () => {
-  const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
+  const response = await axios.post(`${USERS_API}/profile`);
   return response.data;
 };
 
 export const signout = async () => {
-  const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
+  const response = await axios.post(`${USERS_API}/signout`);
   return response.data;
 };
