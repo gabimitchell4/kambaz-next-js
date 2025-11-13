b4248e40fcb55db55961e5b8b3e038043aa7e443
gabimitchell4 <gabimitchell77@gmail.com>
2025-11-13 14:20:43 -0500

    a5

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -48,7 +48,7 @@ export default function Signin() {
         {" "}
         Sign in{" "}
       </Button>
-      <Link id="wd-signup-link" href="/Kambaz/Account/Signup">
+      <Link id="wd-signup-link" href="/Account/Signup">
         {" "}
         Sign up{" "}
       </Link>
