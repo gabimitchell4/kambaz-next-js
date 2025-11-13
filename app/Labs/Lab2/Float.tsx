6bcab068fced003039f97145a51cf435d41388d1
gabimitchell4 <gabimitchell77@gmail.com>
2025-11-13 13:53:00 -0500

    a5

--- app/Labs/Lab2/Float.tsx
+++ app/Labs/Lab2/Float.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+
 export default function Float() {
   return (
     <div id="wd-float-divs">
--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import "./index.css";
 import BackgroundColors from "./BackgroundColors";
 import Borders from "./Borders";
